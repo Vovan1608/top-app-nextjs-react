8780c69587de2a2d0233313f1cf46cf97ab8acf5
Vladymyr Zenin <vzeni@softserveinc.com>
2022-06-23 12:30:20 +0300

    add styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head';
 import { AppProps } from '../node_modules/next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>My Top App</title>
+				<link rel="stylesheet" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
